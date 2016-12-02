003ce36296f3f2fa619fd6fb857bc4708a065442
aminhp93 <minhpn.org.ec@gmail.com>
2016-12-02 11:58:22 -0800

    using and creating validators

--- learning_site/learning_site/forms.py
+++ learning_site/learning_site/forms.py
@@ -3,5 +3,12 @@ from django import forms
 class SuggestionForm(forms.Form):
 	name = forms.CharField()
 	email = forms.EmailField()
-	suggestion = forms.CharField(widget = forms.Textarea)\
+	suggestion = forms.CharField(widget = forms.Textarea)
+	honeypot = forms.CharField(required=False, widget= forms.HiddenInput, label='Leave empty')
+
+	def clean_honeypot(self):
+		honeypot = self.cleaned_data['honeypot']
+		if len(honeypot):
+			raise forms.ValidationError("honeypot hsould be left empty. Bad pot")
+		return honeypot
 
--- learning_site/learning_site/views.py
+++ learning_site/learning_site/views.py
@@ -1,5 +1,31 @@
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 from django.shortcuts import render
+
+from . import forms
+
 
 
 def hello_world(request):
     return render(request, 'home.html')
+
+def suggestion_view(request):
+	form = forms.SuggestionForm()
+
+	if request.method == 'POST':
+		form = forms.SuggestionForm(request.POST)
+		if form.is_valid():
+			print("good form")
+			send_mail(
+				'Suggestion form {}'.format(form.cleaned_data['name']),
+				form.cleaned_data['suggestion'],
+				'{name} <{email}>'.format(**form.cleaned_data),
+				['minh@gmail.com']
+			)
+			messages.add_message(request, messages.SUCCESS, 'Thank for your suggestion')
+			return HttpResponseRedirect(reverse('suggestion'))
+
+	return render(request, 'suggestion_form.html', {'form': form})
